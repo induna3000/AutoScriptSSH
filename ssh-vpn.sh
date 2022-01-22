fe2c536f937cb8cc880fd7460af48d193447e939
induna3000 <97850444+induna3000@users.noreply.github.com>
2022-01-23 00:48:33 +0200

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -21,7 +21,7 @@ commonname=lostserver.xyz
 email=admin@lostserver.xyz
 
 # simple password minimal
-wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/password"
+wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/password"
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -93,15 +93,15 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Setup by LostServer</pre>" > /home/vps/public_html/index.html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/vps.conf"
 /etc/init.d/nginx restart
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://github.com/syntax-er0r/AutoScriptSSH/raw/main/badvpn-udpgw64"
+wget -O /usr/bin/badvpn-udpgw "https://github.com/induna3000/AutoScriptSSH/raw/main/badvpn-udpgw64"
 chmod +x /usr/bin/badvpn-udpgw
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
@@ -125,7 +125,7 @@ echo "/usr/sbin/nologin" >> /etc/shells
 # install squid
 cd
 apt -y install squid3
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/squid3.conf"
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf
 
 # setting vnstat
@@ -180,7 +180,7 @@ sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
 
 #OpenVPN
-wget https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+wget https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
 
 # install fail2ban
 apt -y install fail2ban
@@ -217,7 +217,7 @@ cd
 apt install -y libxml-parser-perl
 
 # banner /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/bannerssh.conf"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/bannerssh.conf"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
@@ -244,25 +244,25 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -O add-host "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addhost.sh"
-wget -O about "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/about.sh"
-wget -O menu "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/menu.sh"
-wget -O usernew "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/trial.sh"
-wget -O hapus "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/hapus.sh"
-wget -O member "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/member.sh"
-wget -o webmin "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/webmin.sh"
-wget -O delete "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/delete.sh"
-wget -O cek "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cek.sh"
-wget -O restart "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/restart.sh"
-wget -O speedtest "https://github.com/syntax-er0r/AutoScriptSSH/raw/main/speedtest_cli.py"
-wget -O info "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/info.sh"
-wget -O ram "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/ram.sh"
-wget -O renew "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renew.sh"
-wget -O autokill "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/tendang.sh"
-wget -O clear-log "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/clear-log.sh"
+wget -O add-host "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/addhost.sh"
+wget -O about "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/about.sh"
+wget -O menu "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/member.sh"
+wget -o webmin "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/webmin.sh"
+wget -O delete "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/delete.sh"
+wget -O cek "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/cek.sh"
+wget -O restart "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/restart.sh"
+wget -O speedtest "https://github.com/induna3000/AutoScriptSSH/raw/main/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/info.sh"
+wget -O ram "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/ram.sh"
+wget -O renew "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/renew.sh"
+wget -O autokill "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/tendang.sh"
+wget -O clear-log "https://raw.githubusercontent.com/induna3000/AutoScriptSSH/main/clear-log.sh"
 
 echo "0 5 * * * root clear-log && reboot" >> /etc/crontab
 
